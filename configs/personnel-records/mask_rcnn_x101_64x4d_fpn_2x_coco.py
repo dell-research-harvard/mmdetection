45c1d8791da2d19586f5c3cfa0836edad52f1637
Krishna Prasad Srinivasan <51783800+krishnapsrinivasan@users.noreply.github.com>
2021-10-27 19:59:14 -0400

    new pretrained model

--- configs/personnel-records/mask_rcnn_x101_64x4d_fpn_2x_coco.py
+++ configs/personnel-records/mask_rcnn_x101_64x4d_fpn_2x_coco.py
@@ -35,7 +35,7 @@ data = dict(
         ann_file='data/input/test10.json'))
 
 # We can use the pre-trained Mask RCNN model to obtain higher performance
-load_from = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'
+load_from = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.5/swin_tiny_c24_patch4_window8_256.pth'
 
 optimizer = dict(type='SGD', lr=0.0002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
