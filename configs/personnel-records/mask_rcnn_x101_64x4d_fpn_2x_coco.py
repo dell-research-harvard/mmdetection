db637f587b98d25c243cf1bd24e7479e2506f403
Krishna Prasad Srinivasan <51783800+krishnapsrinivasan@users.noreply.github.com>
2021-10-27 19:24:39 -0400

    use pre-trained weighst

--- configs/personnel-records/mask_rcnn_x101_64x4d_fpn_2x_coco.py
+++ configs/personnel-records/mask_rcnn_x101_64x4d_fpn_2x_coco.py
@@ -1,4 +1,4 @@
-_base_ = './mask_rcnn_x101_32x4d_fpn_2x_coco.py'
+_base_ = '../mask_rcnn/mask_rcnn_x101_32x4d_fpn_2x_coco.py'
 model = dict(
     backbone=dict(
         type='ResNeXt',
@@ -12,3 +12,30 @@ model = dict(
         style='pytorch',
         init_cfg=dict(
             type='Pretrained', checkpoint='open-mmlab://resnext101_64x4d')))
+
+# We also need to change the num_classes in head to match the dataset's annotation
+# model = dict(
+#     roi_head=dict(
+#         bbox_head=dict(num_classes=1),
+#         mask_head=dict(num_classes=1)))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('character',)
+data = dict(
+    train=dict(
+        img_prefix='data/generated/',
+        classes=classes,
+        ann_file='data/input/train90.json'),
+    val=dict(
+        img_prefix='data/generated/',
+        classes=classes,
+        ann_file='data/input/train90_val10.json'),
+    test=dict(
+        img_prefix='data/generated/',
+        classes=classes,
+        ann_file='data/input/test10.json'))
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'checkpoints/swin_large_patch4_window12_384_22k.pth'
+
