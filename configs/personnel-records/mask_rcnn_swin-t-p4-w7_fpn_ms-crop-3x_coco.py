55cade22509a71ec3ca02fbbf4a8386de3f01c91
Krishna Prasad Srinivasan <51783800+krishnapsrinivasan@users.noreply.github.com>
2021-10-30 10:48:23 -0400

    Update mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py

--- configs/personnel-records/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
+++ configs/personnel-records/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco.py
@@ -4,14 +4,14 @@ _base_ = [
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
-pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_tiny_patch4_window7_224.pth'  # noqa
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_base_patch4_window7_224_22k.pth'  # noqa
 
 model = dict(
     type='MaskRCNN',
     backbone=dict(
         _delete_=True,
         type='SwinTransformer',
-        embed_dims=96,
+        embed_dims=144,
         depths=[2, 2, 6, 2],
         num_heads=[3, 6, 12, 24],
         window_size=7,
